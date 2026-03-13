768d3da6a5e694b88278c878250521644e12921b
ruinunes <rui.r.b.nunes@gmail.com>
2026-03-13 14:07:10 +0000

    build: create docker compose pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
+import { environment } from './environments/environment.development';
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+async function loadUsers() {
+
+  const response = await fetch(environment.apiUrl + '/movies');
+  const data = await response.json();
+
+  console.log(data);
+}
+
+loadUsers();
